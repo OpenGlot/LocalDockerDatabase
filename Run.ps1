102ebbb2098414fed978bcbf97456c9a19304165
paul totzke <7072k3@gmail.com>
2024-06-16 22:58:15 -0500

    New Init Script

--- Run.ps1
+++ Run.ps1
@@ -1,3 +1,5 @@
-docker rm my_postgres_container
+docker rm -f my_postgres_container
+docker volume prune
 docker build -t my_postgres_image .
 docker run --name my_postgres_container -d -p 6543:5432 my_postgres_image
+docker logs my_postgres_container
